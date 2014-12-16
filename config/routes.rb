f24fc63dfdf22345130bf5b33cc0ba661044bf66
Devin Flynn <dfloveone@gmail.com>
2014-12-16 14:46:21 -0500

    live

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
   
-   
+  root 'static_pages#fantasy_basketball'
+
+ 
     get 'fantasy_basketball' => 'static_pages#fantasy_basketball'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
